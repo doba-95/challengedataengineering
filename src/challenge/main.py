08a822724d113a19ad64e4293859db15a290fcb6
Dominic <dominic.bachmann@haeger-consulting.de>
2026-01-30 19:04:37 +0100

    Running batch mode with memory controll

--- src/challenge/main.py
+++ src/challenge/main.py
@@ -2,18 +2,20 @@ import logging
 import multiprocessing
 import os
 import time
-
+import pandas as pd
 from src.challenge.common.generated_file_cleaner import remove_parquet_files
 from src.challenge.profit.data.data_transformer import (
-    print_best_worst_performing_product,
+    batch_reader_parquet_files,
 )
 from src.challenge.profit.profit_etl import ProfitEtl
 
 start = time.time()
 
 TRANSACTIONS_DIR = "./csv_files/transactions"
+PARQUET_DIR = "./csv_files/transactions/parquet_transactions"
 
-transactions = [f for f in os.listdir(TRANSACTIONS_DIR) if f.endswith('.csv')]
+transactions = [f for f in os.listdir(TRANSACTIONS_DIR) if f.endswith(".csv")]
+
 
 def etl_runner(transaction: str):
     profit_etl = ProfitEtl(transaction)
@@ -25,8 +27,6 @@ def etl_runner(transaction: str):
             break
 
 
-
-
 if __name__ == "__main__":
     logging.basicConfig(filename="challenge.log", level=logging.INFO)
     ## Run etl pipeline with multiple cores
@@ -34,11 +34,39 @@ if __name__ == "__main__":
         pool.map(etl_runner, transactions)
 
     ## Print best/worst performing products
-    transactions_parquets = [f for f in os.listdir(TRANSACTIONS_DIR) if f.endswith('.parquet')]
-    #with multiprocessing.Pool(6) as pool:
+    transactions_parquets = [
+        f for f in os.listdir(TRANSACTIONS_DIR) if f.endswith(".parquet")
+    ]
+    # with multiprocessing.Pool(6) as pool:
 
-    print_best_worst_performing_product(transactions_parquets)
+    processed_batch = batch_reader_parquet_files(PARQUET_DIR)
+
+    final_columns = ["product_id", "amount_usd", "production_costs", "profit", "product_name"]
+    final_df = pd.DataFrame(columns=final_columns)
+
+    # for batch_df in next(processed_batch):
+    #     if batch_df is StopIteration:
+    #         break
+    batches = 0
+    while True:
+        try:
+            batches += 1
+            batch_df = next(processed_batch)
+            final_df = pd.concat([final_df, batch_df])
+            final_df.groupby(by="product_id").agg(
+                {
+                    "product_id": "first",
+                    "amount_usd": "sum",
+                    "production_costs": "sum",
+                    "profit": "sum",
+                    "product_name": "first",
+                }
+            )
+        except StopIteration:
+            break
+
+    print(final_df.sort_values(by=["profit"], ascending=False).iloc[[0,-1]])
 
 
-remove_parquet_files(TRANSACTIONS_DIR)
+remove_parquet_files(PARQUET_DIR)
 print(time.time() - start)
--- src/challenge/profit/data/data_transformer.py
+++ src/challenge/profit/data/data_transformer.py
@@ -30,8 +30,8 @@ def calculate_profit(df):
     return df
 
 
-def print_best_worst_performing_product(parquet_files):
-    datasets = ds.dataset("./csv_files/transactions/parquet_transactions/")
+def batch_reader_parquet_files(parquet_path):
+    datasets = ds.dataset(parquet_path)
     columns_of_interest = [
         "product_id",
         "product_name",
@@ -39,13 +39,19 @@ def print_best_worst_performing_product(parquet_files):
         "production_costs",
         "profit",
     ]
-    for batch in datasets.to_batches(columns=columns_of_interest, batch_size=100000):
+    for batch in datasets.to_batches(columns=columns_of_interest, batch_size=1000000, use_threads=True):
         batch_df = batch.to_pandas()
-        print(batch_df.groupby(by="product_id").agg(
-            {
-                "amount_usd": "sum",
-                "production_costs": "sum",
-                "profit": "sum",
-                "product_name": "first",
-            }
-        ).sort_values(by=["profit"], ascending=False).iloc[[0, -1]])
+        yield aggregation_of_columns(batch_df)
+
+
+def aggregation_of_columns(df):
+    return_df = df.groupby(by="product_id").agg(
+        {
+            "product_id": "first",
+            "amount_usd": "sum",
+            "production_costs": "sum",
+            "profit": "sum",
+            "product_name": "first",
+        }
+    )
+    return return_df
